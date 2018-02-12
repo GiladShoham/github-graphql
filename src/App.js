5c925dac4df7dd7c630bc49010daed8871db7113
Gilad Shoham <shoham.gilad@gmail.com>
2018-02-12 17:20:27 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import {
+  ApolloProvider,
+} from 'react-apollo';
+import './App.css';
+
+import createClient from '@bit/giladshoham.test4.apollo.create-client';
+import RepoInfoWithData from '@bit/giladshoham.test4.react.repo-info-issues';
+
+const token = 'YOUR TOEKN HERE';
+const client = createClient(token);
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>GraphQL + Bit = ♥ </h2>
+          </div>
+          <RepoInfoWithData owner="teambit" name="bit"/>
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
